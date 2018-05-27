b0e6ff0f8e8ab23dd36b8f5878f1faf1d846e4e6
isaiahinocentes <isaiahpaulinocentes@gmail.com>
2018-05-27 19:08:21 +0800

    Added Book Model, Controller, and View for Data Table viewing Books

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Book;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +24,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $books = Book::all();
+        return view('home')
+            ->with("books", $books);
     }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,38 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-12 books_table">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">Books</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <table id="books_table" class="table table-striped table-bordered" style="width:100%">
+                        <thead>
+                            <tr>
+                                <th>ID</th>
+                                <th>Title</th>
+                                <th>Name</th>
+                                <th>Author</th>
+                                <th>Categories</th>
+                                <th>Date</th>
+                                <th>Added By</th>
+                                <th>Created Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @foreach($books as $book)
+                                <tr>
+                                    <td>{{$book->id}}</td>
+                                    <td>{{$book->title}}</td>
+                                    <td>{{$book->name}}</td>
+                                    <td>{{$book->author}}</td>
+                                    <td>{{$book->categories}}</td>
+                                    <td>{{$book->date}}</td>
+                                    <td>{{$book->added_by}}</td>
+                                    <td>{{$book->created_at}}</td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
