b5375289e3ba9f86bb6d43a39efac4480e10a043
Michael <michaeldy.bautista@gmail.com>
2018-05-30 22:57:47 +0800

    admin-lte

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,43 +1,108 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          
+          <div class="col-sm-12">
+            <ol class="breadcrumb float-sm-right">
+              <li class="breadcrumb-item"><a href="#">Home</a></li>
+              <li class="breadcrumb-item active">Starter Page</li>
+            </ol>
+          </div><!-- /.col -->
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+ </div>
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-12 books_table">
-            <div class="card">
-                <div class="card-header">Books</div>
-                <div class="card-body">
-                    <table id="books_table" class="table table-striped table-bordered" style="width:100%">
-                        <thead>
-                            <tr>
-                                <th>ID</th>
-                                <th>Title</th>
-                                <th>Name</th>
-                                <th>Author</th>
-                                <th>Categories</th>
-                                <th>Date</th>
-                                <th>Added By</th>
-                                <th>Created Date</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach($books as $book)
-                                <tr>
-                                    <td>{{$book->id}}</td>
-                                    <td>{{$book->title}}</td>
-                                    <td>{{$book->name}}</td>
-                                    <td>{{$book->author}}</td>
-                                    <td>{{$book->categories}}</td>
-                                    <td>{{$book->date}}</td>
-                                    <td>{{$book->added_by}}</td>
-                                    <td>{{$book->created_at}}</td>
-                                </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
-                </div>
-            </div>
-        </div>
+<div class="card">
+    <div class="card-header">
+        <h5 class="card-title">Books</h5>
+    </div>
+    <div class="card-body">
+            <table id="booksTable" class="table table-bordered table-striped">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Book Title</th>
+                        <th>Author</th>
+                        <th>Categories</th>
+                        <th>Date</th>
+                        <th>Added By</th>
+                        <th>Date Created</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                        <td>1</td>
+                    </tr>
+                </tbody>
+            </table>
     </div>
 </div>
+</div>
+@endsection
+@section('scripts')
+    <script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+    <script>
+        $('booksTable').DataTable();
+    </script>
 @endsection
